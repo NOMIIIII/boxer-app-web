af7ff5b448b6a6b823c0b2a1e9c047b22b0c600e
Nauman Ahmed <35535536+nauman-ahmed@users.noreply.github.com>
2023-06-22 14:15:03 +0200

    Updated seconds settings

--- src/screens/newFight.js
+++ src/screens/newFight.js
@@ -92,20 +92,26 @@ export default function NewFight() {
                     playbackRef.current = playbackRef.current + 0.5
                 }
                 if (e.code === "ArrowRight") {
-                    let forward  = !playingRef.current ? 1 : 1
-                    // let forward  = 1
-                    playerRef.current.seekTo(progressRef.current[playIndexRef.current] + forward)
-                    progressRef.current[playIndexRef.current] = progressRef.current[playIndexRef.current] + forward
-                    // setProgress(progress + 1)
+                    if(!playingRef.current){
+                        playerRef.current.seekTo(progressRef.current[playIndexRef.current] + 0.1)
+                        progressRef.current[playIndexRef.current] = progressRef.current[playIndexRef.current] + 0.1
+                    }else{
+                        let forward  = !playingRef.current ? 1 : 1
+                        playerRef.current.seekTo(progressRef.current[playIndexRef.current] + 0.1)
+                        progressRef.current[playIndexRef.current] = progressRef.current[playIndexRef.current] + 0.1
+                    }
                 }
                 if (e.code === "ArrowLeft") {
                     let backward  = !playingRef.current ? 1 : 1
-                    // let backward  = 1
                     if(progressRef.current[playIndexRef.current] - backward > 0){
-                        playerRef.current.seekTo(progressRef.current[playIndexRef.current] - backward)
-                        progressRef.current[playIndexRef.current] = progressRef.current[playIndexRef.current] - backward
+                        if(!playingRef.current){
+                            playerRef.current.seekTo(progressRef.current[playIndexRef.current] - 0.1)
+                            progressRef.current[playIndexRef.current] = progressRef.current[playIndexRef.current] - 0.1
+                        }else{
+                            playerRef.current.seekTo(progressRef.current[playIndexRef.current] - backward)
+                            progressRef.current[playIndexRef.current] = progressRef.current[playIndexRef.current] - backward
+                        }
                     }
-                    // setProgress(progress - 1)
                 }
                 activeTextBoxRef.current = false
             }
@@ -208,8 +214,6 @@ export default function NewFight() {
         extractFrames(f)
     }
     
-    
-
     const getBase64FromUrl = async (dataurl) => { // BASE&$ CONVERStION 
         if(dataurl){
             // console.log("getBase64FromUrl",dataurl)
@@ -608,7 +612,7 @@ export default function NewFight() {
                             // light = {true}
                             // id='ReactPlayer'
                             playing={playing}
-                            // controls={true}
+                            controls={true}
                             width="379"
                             height="300"
                             // onPlay={() => setVideoPaused(false)}
